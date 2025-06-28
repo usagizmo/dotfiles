7f4f24069f5853a41bf2c52caa57389c1ca001f9
usagizmo <usagizmo@gmail.com>
2025-06-29 08:29:04 +0900

    ⚙️ NeovimのinsertモードにEmacs風カーソル移動を追加
    - Ctrl+a/e: 行頭/行末移動
    - Ctrl+f/b: 前進/後退
    - Ctrl+n/p: 上下移動
    - Ctrl+d/h/k: 削除系操作

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,17 @@
 -- 基本的なキーマップ
 vim.keymap.set("i", "jk", "<Esc>")
 
+-- Insert モードでの Emacs 風カーソル移動
+vim.keymap.set("i", "<C-a>", "<Home>", { desc = "Move to beginning of line" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "Move to end of line" })
+vim.keymap.set("i", "<C-f>", "<Right>", { desc = "Move forward" })
+vim.keymap.set("i", "<C-b>", "<Left>", { desc = "Move backward" })
+vim.keymap.set("i", "<C-n>", "<Down>", { desc = "Move down" })
+vim.keymap.set("i", "<C-p>", "<Up>", { desc = "Move up" })
+vim.keymap.set("i", "<C-d>", "<Del>", { desc = "Delete character" })
+vim.keymap.set("i", "<C-h>", "<BS>", { desc = "Backspace" })
+vim.keymap.set("i", "<C-k>", "<C-o>D", { desc = "Kill line" })
+
 -- Neo-tree
 vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>", { desc = "NeoTree" })
 
