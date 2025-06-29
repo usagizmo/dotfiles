000a19bca8b457ab5a083b5d0e6726adae138086
usagizmo <usagizmo@gmail.com>
2025-06-29 09:10:47 +0900

    ⚙️ Neovimのキーバインドを簡略化 - Neo-treeのキーバインドを<leader>eに変更 - Telescopeのキーバインドを簡略化（<leader>f接頭辞を削除） - buffersとhelp_tagsのキーバインドを削除

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,12 +15,10 @@ vim.keymap.set("i", "<C-h>", "<BS>", { desc = "Backspace" })
 vim.keymap.set("i", "<C-k>", "<C-o>D", { desc = "Kill line" })
 
 -- Neo-tree
-vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>", { desc = "NeoTree" })
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus<cr>", { desc = "NeoTree" })
 
 -- Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
-vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
-vim.keymap.set("n", "<leader>fc", "<cmd>Telescope commands<cr>", { desc = "Commands" })
+vim.keymap.set("n", "<leader>o", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>r", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>c", "<cmd>Telescope commands<cr>", { desc = "Commands" })
